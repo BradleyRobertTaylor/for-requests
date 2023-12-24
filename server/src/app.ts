70886310e3383139a5cc01df846ba87be5152221
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2023-12-24 11:14:05 -0800

    Add express rate limiter

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,11 +6,13 @@ import binRoutes from './routes/binRoutes';
 import sseRoute from './routes/sseRoute';
 import { notFound } from './middleware/notFound';
 import { errorHandler } from './middleware/errorHandler';
+import { limiter } from './middleware/rateLimiter';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(limiter);
 
 app.use('/api/bins', binRoutes);
 app.use('/api/subscribe', sseRoute);
