b56219f8bae4ed76ee96010e21f6085d0bcd6211
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2023-12-27 10:17:55 -0800

    Set x-accel-buffering header for nginx

--- server/src/controllers/sseController.ts
+++ server/src/controllers/sseController.ts
@@ -13,6 +13,7 @@ export const sseController = (req: Request, res: Response) => {
     'Content-Type': 'text/event-stream',
     Connection: 'keep-alive',
     'Cache-Control': 'no-cache',
+    'X-Accel-Buffering': 'no',
   };
   res.writeHead(200, headers);
 
