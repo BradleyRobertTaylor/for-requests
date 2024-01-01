9ec126c77ad21ab19896a731794f6eb14114c707
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2024-01-01 12:30:43 -0800

    Small refactor

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,13 @@ import app from './app';
 import { connectDB } from './db/connectDB';
 import { env } from './env';
 
-const startServer = async () => {
+async function startServer() {
   await connectDB();
 
   const PORT = env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`);
   });
-};
+}
 
 startServer();
--- server/src/utils/helpers.ts
+++ server/src/utils/helpers.ts
@@ -1,17 +1,18 @@
 import { Bin } from '../models/Bin';
 import { Event } from '../models/Event';
 
-export const parsePath = (path: string, binPath: string) => {
+export function parsePath(path: string, binPath: string) {
   const parsedPath = path.split(`/${binPath}`).join('');
   return parsedPath === '' ? '/' : parsedPath;
-};
+}
 
-export const removeEventId = (events: Event[]) =>
-  events.map(({ id, ...rest }) => rest);
+export function removeEventId(events: Event[]) {
+  return events.map(({ id, ...rest }) => rest);
+}
 
-export const removeBinId = (bin: Bin | null): Omit<Bin, 'id'> | undefined => {
+export function removeBinId(bin: Bin | null): Omit<Bin, 'id'> | undefined {
   if (!bin) return;
 
   const { id, ...rest } = bin;
   return rest;
-};
+}
